a07391a6af2c547a61387ed464fa60db99b842f7
Isa-julie <j.isaro@alustudent.com>
2024-10-24 02:45:29 -0700

    g

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str1 = "object-oriented "
-str2 = "programming "
-str3 = "with "
-str4 = "Python"
-print(str1 + str2 + str3 + str4)
+str2 = "programming with "
+print(str1 + str2, end="")
+print("Python")
