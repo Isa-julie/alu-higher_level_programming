561e242a8776326551652411495c667c2932f769
Isa-julie <j.isaro@alustudent.com>
2024-10-24 02:22:16 -0700

    f

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import sys
-str1 = "Welcome to"
-str2 = "Holberton School!"
-print(f"{str1} {str2}", file=sys.stderr)
+import sys; str1 = "Welcome to"; str2 = "Holberton School!"
+print(str1 + " " + str2)
+sys.stderr.write("This is an error message\n")
