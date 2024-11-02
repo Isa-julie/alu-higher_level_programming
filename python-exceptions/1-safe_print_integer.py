1e8cb7938d6fdfdd3c0bf5ce7aefe369b439f11f
Isa-julie <j.isaro@alustudent.com>
2024-11-02 03:38:19 -0700

    f

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))  # Try to format and print the value as an integer
-        return True  # Return True if it worked without error
+        print("{:d}".format(value))  
+        return True  
     except (ValueError, TypeError):
-        return False  # Return False if formatting fails
-
+        return False  
--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 def list_division(my_list_1, my_list_2, list_length):
     result_list = []
-    
     for i in range(list_length):
         try:
             num1 = my_list_1[i]
@@ -19,5 +17,4 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             result_list.append(result)
-    
     return result_list
