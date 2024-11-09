8bfe01087b1eab4f98f4df432d8dc4c6a945a7df
Isa-julie <j.isaro@alustudent.com>
2024-11-09 08:59:00 -0800

    j

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -1,32 +1,54 @@
 #!/usr/bin/python3
-"""This module defines a Rectangle class with width, height, methods
-for area, perimeter, and string representations, along with tracking
-instances and customizing the print symbol.
-"""
+"""Define a rectangle"""
 
 
 class Rectangle:
-    """Defines a rectangle with width, height, and methods for area,
-    perimeter, and string representations.
-    """
+    """Creates a rectangle."""
 
-    number_of_instances = 0  # Class attribute to track the number of instances
-    print_symbol = "#"       # Class attribute to store the symbol for string
-    
+    number_of_instances = 0
+    print_symbol = '#'
+
     def __init__(self, width=0, height=0):
-        """Initialize the rectangle with optional width and height."""
+        """Initialize data."""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
+    def __str__(self):
+        """Returns the rectangle with the character #"""
+        if self.__width == 0 or self.__height == 0:
+            return ''
+        rectangle = ''
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rectangle += str(self.print_symbol)
+            rectangle += '\n'
+        return rectangle[:-1]
+
+    def __repr__(self):
+        """Return a string representation of the rectangle to be able
+        to recreate a new instance by using eval() (see example below)
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Print the message Bye rectangle...
+        (... being 3 dots not ellipsis) when an
+        instance of Rectangle is deleted"""
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
     @property
     def width(self):
-        """Retrieve the width of the rectangle."""
+        """Retrieves the width."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the width of the rectangle."""
+        """Sets the width of a Rectangle instance
+        Args:
+            value: value of width, must be >=0
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -35,12 +57,15 @@ class Rectangle:
 
     @property
     def height(self):
-        """Retrieve the height of the rectangle."""
+        """Retrieves the height."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height of the rectangle."""
+        """Sets the height of a Rectangle instance
+        Args:
+            value: value of height, must be >=0
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -48,26 +73,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Return the area of the rectangle."""
+        """returns the rectangle area."""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Return the perimeter of the rectangle."""
+        """Returns the rectangle perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-
-    def __str__(self):
-        """Return a string representation of the rectangle using print_symbol."""
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        return "\n".join([str(self.print_symbol) * self.__width] * self.__height)
-
-    def __repr__(self):
-        """Return a string representation to recreate the rectangle."""
-        return f"Rectangle({self.__width}, {self.__height})"
-
-    def __del__(self):
-        """Print a message when the instance is deleted."""
-        Rectangle.number_of_instances -= 1
-        print("Bye rectangle...")
