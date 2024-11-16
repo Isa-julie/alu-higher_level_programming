a2193468f871f2666bcbcc8c4a01e3e4bf8ffa6c
Isa-julie <j.isaro@alustudent.com>
2024-11-15 18:25:22 -0800

    f

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,2 @@
 -- Select all cities in California from the database hbtn_0d_usa.
-USE hbtn_0d_usa;
-
-SELECT name
-FROM cities
-WHERE state_id = (
-    SELECT id FROM states WHERE name = 'California'
-)
-ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,6 @@
 -- List all cities and their associated states in the database hbtn_0d_usa.
-USE hbtn_0d_usa;
-SELECT
-    cities.id AS city_id,
-    cities.name AS city_name,
-    (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities
-ORDER BY cities.id ASC;
+INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id
