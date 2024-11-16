fae126d55dc5f9764b2bb6f4fced1729b614ed52
Isa-julie <j.isaro@alustudent.com>
2024-11-15 18:39:35 -0800

    d

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
--- List all genres of the show "Dexter" in the database hbtn_0d_tvshows.
+--  lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT tv_shows.title
 FROM tv_shows
 INNER JOIN tv_show_genres
