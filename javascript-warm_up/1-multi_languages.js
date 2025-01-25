d6757530bdcc98bbae834b544e3667ae23d29bb9
Isa-julie <j.isaro@alustudent.com>
2025-01-25 12:18:18 -0800

    array

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 
-let messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
 console.log(messages.join("\n"));
-
