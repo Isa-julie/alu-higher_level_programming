ab6df5e7876b3451c5f2455193be9c6b07711b39
Isa-julie <j.isaro@alustudent.com>
2025-01-28 07:40:51 -0800

    replacing

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
-
-const args = process.argv.slice(2).map(Number); // Convert arguments to integers
-
-if (args.length < 2) {
-  console.log(0);
-} else {
-  args.sort((a, b) => b - a); // Sort the array in descending order
-  const index = args.indexOf(12); // Find the index of 12
-  if (index !== -1) {
-    args[index] = 89; // Replace 12 with 89
-  }
-  console.log(args[1]); // Print the second biggest integer
-}
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
